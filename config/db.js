a7249ec987b68e4ff85f8766566e9d8647e31065
bgeils <bgeils@iastate.edu>
2017-11-04 21:52:41 -0500

    new env var

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
 
 module.exports = {
-  db: 'mongodb://localhost/open-energy'
+  db: process.env.OPENENERGYPRODUCTIONDB
 };

79d302adadad05820cc0b309eda68fb389291ecc
bgeils <bgeils@iastate.edu>
2017-11-23 15:52:57 -0600

    push

--- server.js
+++ server.js
@@ -80,6 +80,8 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://dev-357313.oktapreview.com/api/v1/authn');
+    
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
